55df064cd59d7e7118f57e07712c65ba6b92fead
Ari Adamo <anarianna657@gmail.com>
2025-09-09 13:30:26 -0400

    right

--- src/project/annotations/BadCode.java
+++ src/project/annotations/BadCode.java
@@ -1,7 +1,7 @@
 package project.annotations;
 
-public class hi {	
+public class BadCode {	
 	public static void main(String[] args) {
-		System.out.println("Hello world")
+		System.out.println("Hello world");
 	}
 }
